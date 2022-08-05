5b421368c29df793f5d68f57e2d3545f75ded892
Alok Singh Chauhan <alokchauhan.kalpi@gmail.com>
2022-08-05 14:21:13 +0530

    Fixed UI for login pages

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-include 'inc/header.php';
+include 'inc/login_header.php';
 Session::CheckLogin();
 ?>
 
@@ -22,39 +22,66 @@ if (isset($logout)) {
 
  ?>
 
-<div class="card ">
-  <div class="card-header">
-          <h3 class='text-center'><i class="fas fa-sign-in-alt mr-2"></i>User login</h3>
-        </div>
-        <div class="card-body">
 
-
-            <div style="width:450px; margin:0px auto">
-
-            <form class="" action="" method="post">
-                <div class="form-group">
-                  <label for="email">Email address</label>
-                  <input type="email" name="email"  class="form-control">
+<!----   --->
+ <main class="main-content  mt-0">
+    <section>
+      <div class="page-header min-vh-100">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
+              <div class="card card-plain">
+                <div class="card-header pb-0 text-start">
+                  <h4 class="font-weight-bolder">Sign In</h4>
+                  <p class="mb-0">Enter your email and password to sign in</p>
                 </div>
-                <div class="form-group">
-                  <label for="password">Password</label>
-                  <input type="password" name="password"  class="form-control">
+                <div class="card-body">
+                  <form role="form" class="" action="" method="post">
+                    <div class="mb-3">
+                      <input type="email" name="email" class="form-control form-control-lg" placeholder="Email" aria-label="Email">
+                    </div>
+                    <div class="mb-3">
+                      <input type="password" name="password" class="form-control form-control-lg" placeholder="Password" aria-label="Password">
+                    </div>
+                    <div class="text-center">
+                      <button type="submit" name="login" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Sign in</button>
+                    </div>
+                  </form>
                 </div>
-                <div class="form-group">
-                  <button type="submit" name="login" class="btn btn-success">Login</button>
-                </div>
-
-
-            </form>
+                
+              </div>
+            </div>
+            <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg');
+          background-size: cover;">
+                <span class="mask bg-gradient-primary opacity-6"></span>
+                <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new currency"</h4>
+                <p class="text-white position-relative">The more effortless the writing looks, the more effort the writer actually put into the process.</p>
+              </div>
+            </div>
           </div>
-
-
         </div>
       </div>
+    </section>
+  </main>
+  <!--   Core JS Files   -->
+  <script src="theme/assets/js/core/popper.min.js"></script>
+  <script src="theme/assets/js/core/bootstrap.min.js"></script>
+  <script src="theme/assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="theme/assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="theme/assets/js/argon-dashboard.min.js?v=2.0.4"></script>
+</body>
 
-
-
-  <?php
-  include 'inc/footer.php';
-
-  ?>
+</html>
