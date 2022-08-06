44abb8fb465cb6e38adba185f4d7003129bd0465
Alok Singh Chauhan <alokchauhan.kalpi@gmail.com>
2022-08-06 10:02:27 +0530

    Re-started the project

--- config/config.php
+++ config/config.php
@@ -1,7 +1,29 @@
 <?php
 
+//Note: This file should be included first in every php page.
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+define('BASE_PATH', dirname(dirname(__FILE__)));
+define('APP_FOLDER', 'simpleadmin');
+define('CURRENT_PAGE', basename($_SERVER['REQUEST_URI']));
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'insurance');
-define('DB_USER', 'alok');
-define('DB_PASS', 'Alok@12345');
+require_once BASE_PATH . '/lib/MysqliDb/MysqliDb.php';
+require_once BASE_PATH . '/helpers/helpers.php';
+
+/*
+|--------------------------------------------------------------------------
+| DATABASE CONFIGURATION
+|--------------------------------------------------------------------------
+ */
+
+define('DB_HOST', "localhost");
+define('DB_USER', "alok");
+define('DB_PASSWORD', "Alok@12345");
+define('DB_NAME', "insurance");
+
+/**
+ * Get instance of DB object
+ */
+function getDbInstance() {
+	return new MysqliDb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+}
--- login.php
+++ login.php
@@ -1,87 +1,93 @@
 <?php
-include 'inc/login_header.php';
-Session::CheckLogin();
+session_start();
+require_once 'config/config.php';
+$token = bin2hex(openssl_random_pseudo_bytes(16));
+
+// If User has already logged in, redirect to dashboard page.
+if (isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in'] === TRUE)
+{
+	header('Location:index.php');
+}
+
+// If user has previously selected "remember me option": 
+if (isset($_COOKIE['series_id']) && isset($_COOKIE['remember_token']))
+{
+	// Get user credentials from cookies.
+	$series_id = filter_var($_COOKIE['series_id']);
+	$remember_token = filter_var($_COOKIE['remember_token']);
+	$db = getDbInstance();
+	// Get user By series ID: 
+	$db->where('series_id', $series_id);
+	$row = $db->getOne('admin_accounts');
+
+	if ($db->count >= 1)
+	{
+		// User found. verify remember token
+		if (password_verify($remember_token, $row['remember_token']))
+        	{
+			// Verify if expiry time is modified. 
+			$expires = strtotime($row['expires']);
+
+			if (strtotime(date()) > $expires)
+			{
+				// Remember Cookie has expired. 
+				clearAuthCookie();
+				header('Location:login.php');
+				exit;
+			}
+
+			$_SESSION['user_logged_in'] = TRUE;
+			$_SESSION['admin_type'] = $row['admin_type'];
+			header('Location:index.php');
+			exit;
+		}
+		else
+		{
+			clearAuthCookie();
+			header('Location:login.php');
+			exit;
+		}
+	}
+	else
+	{
+		clearAuthCookie();
+		header('Location:login.php');
+		exit;
+	}
+}
+
+include BASE_PATH.'/includes/header.php';
 ?>
-
-
-<?php
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
-   $userLog = $users->userLoginAuthotication($_POST);
-}
-if (isset($userLog)) {
-  echo $userLog;
-}
-
-$logout = Session::get('logout');
-if (isset($logout)) {
-  echo $logout;
-}
-
-
-
- ?>
-
-
-<!----   --->
- <main class="main-content  mt-0">
-    <section>
-      <div class="page-header min-vh-100">
-        <div class="container">
-          <div class="row">
-            <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
-              <div class="card card-plain">
-                <div class="card-header pb-0 text-start">
-                  <h4 class="font-weight-bolder">Sign In</h4>
-                  <p class="mb-0">Enter your email and password to sign in</p>
-                </div>
-                <div class="card-body">
-                  <form role="form" class="" action="" method="post">
-                    <div class="mb-3">
-                      <input type="email" name="email" class="form-control form-control-lg" placeholder="Email" aria-label="Email">
-                    </div>
-                    <div class="mb-3">
-                      <input type="password" name="password" class="form-control form-control-lg" placeholder="Password" aria-label="Password">
-                    </div>
-                    <div class="text-center">
-                      <button type="submit" name="login" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Sign in</button>
-                    </div>
-                  </form>
-                </div>
-                
-              </div>
-            </div>
-            <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg');
-          background-size: cover;">
-                <span class="mask bg-gradient-primary opacity-6"></span>
-                <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new currency"</h4>
-                <p class="text-white position-relative">The more effortless the writing looks, the more effort the writer actually put into the process.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
-  <!--   Core JS Files   -->
-  <script src="theme/assets/js/core/popper.min.js"></script>
-  <script src="theme/assets/js/core/bootstrap.min.js"></script>
-  <script src="theme/assets/js/plugins/perfect-scrollbar.min.js"></script>
-  <script src="theme/assets/js/plugins/smooth-scrollbar.min.js"></script>
-  <script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-      var options = {
-        damping: '0.5'
-      }
-      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-  </script>
-  <!-- Github buttons -->
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
-  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="theme/assets/js/argon-dashboard.min.js?v=2.0.4"></script>
-</body>
-
-</html>
+<div id="page-" class="col-md-4 col-md-offset-4">
+	<form class="form loginform" method="POST" action="authenticate.php">
+		<div class="login-panel panel panel-default">
+			<div class="panel-heading">Please Sign in</div>
+			<div class="panel-body">
+				<div class="form-group">
+					<label class="control-label">username</label>
+					<input type="text" name="username" class="form-control" required="required">
+				</div>
+				<div class="form-group">
+					<label class="control-label">password</label>
+					<input type="password" name="passwd" class="form-control" required="required">
+				</div>
+				<div class="checkbox">
+					<label>
+						<input name="remember" type="checkbox" value="1">Remember Me
+					</label>
+				</div>
+				<?php if (isset($_SESSION['login_failure'])): ?>
+				<div class="alert alert-danger alert-dismissable fade in">
+					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+					<?php
+					echo $_SESSION['login_failure'];
+					unset($_SESSION['login_failure']);
+					?>
+				</div>
+				<?php endif; ?>
+				<button type="submit" class="btn btn-success loginField">Login</button>
+			</div>
+		</div>
+	</form>
+</div>
+<?php include BASE_PATH.'/includes/footer.php'; ?>
