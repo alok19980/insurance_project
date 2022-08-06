5f7289369fd7db15df927f374985033fd3fd7f13
Alok Singh Chauhan <alokchauhan.kalpi@gmail.com>
2022-08-06 14:26:58 +0530

    Completed Dashboard page

--- index.php
+++ index.php
@@ -7,7 +7,11 @@ require_once 'includes/auth_validate.php';
 $db = getDbInstance();
 
 //Get Dashboard information
-//$numCustomers = $db->getValue ("customers", "count(*)");
+$giCount = $db->getValue ("gi", "count(*)");
+$liCount = $db->getValue ("li", "count(*)");
+$mcCount = $db->getValue ("mi", "count(*)");
+$siCount = $db->getValue ("si", "count(*)");
+$pcCount = $db->getValue ("policyCopy", "count(*)");
 
 include_once('includes/header.php');
 ?>
@@ -48,13 +52,167 @@ include_once('includes/header.php');
     <div class="container-fluid py-4">
       
       <div class="row mt-4" style="color:white; text-align:center;">
-        
         <div class="col-lg-12">
-            <b>
-          "There is nothing left behind if you are here now..."
-</b>
+            <b>"There is nothing left behind if you are here now..."</b>           
         </div>
       </div>
+
+      <div class="container-fluid py-4">
+      <div class="row">
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                  <div class="numbers">
+                    <p class="text-sm mb-0 text-uppercase font-weight-bold">GI</p>
+                    <h5 class="font-weight-bolder">
+                      <?php echo $giCount; ?>
+                    </h5>
+                    
+                  </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                    <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                  <div class="numbers">
+                    <p class="text-sm mb-0 text-uppercase font-weight-bold">LI</p>
+                    <h5 class="font-weight-bolder">
+                    <?php echo $liCount; ?>
+                    </h5>
+                    
+                  </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-danger shadow-danger text-center rounded-circle">
+                    <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                  <div class="numbers">
+                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Mediclaim</p>
+                    <h5 class="font-weight-bolder">
+                    <?php echo $mcCount; ?>
+                    </h5>
+                    
+                  </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
+                    <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6">
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                  <div class="numbers">
+                    <p class="text-sm mb-0 text-uppercase font-weight-bold">SI</p>
+                    <h5 class="font-weight-bolder">
+                    <?php echo $siCount; ?>
+                    </h5>
+                    
+                  </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-warning shadow-warning text-center rounded-circle">
+                    <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-4">
+      <div class="col-lg-5 mb-lg-0 mb-4">
+          <div class="card z-index-2 h-100">
+          <div class="card-header pb-0 pt-3 bg-transparent">
+              <h6 class="text-capitalize">Summary</h6>
+            </div>
+            <div class="card-body p-3">
+              
+              <div>
+                <p>Total Gneral Insurance Count : <?php echo $giCount; ?></p>
+                <p>Total Life Insurance Count : <?php echo $liCount; ?></p>
+                <p>Total Mediclaim Count : <?php echo $mcCount; ?></p>
+                <p>Total Simple Insurance Count : <?php echo $siCount; ?></p>
+                <p>Total Policy Copy Count : <?php echo $pcCount; ?></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-7">
+          <div class="card card-carousel overflow-hidden h-100 p-0">
+            <div id="carouselExampleCaptions" class="carousel slide h-100" data-bs-ride="carousel">
+              <div class="carousel-inner border-radius-lg h-100">
+                <div class="carousel-item h-100 active" style="background-image: url('theme/assets/img/carousel-1.jpg');
+      background-size: cover;">
+                  <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
+                    <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
+                      <i class="ni ni-camera-compact text-dark opacity-10"></i>
+                    </div>
+                    <h5 class="text-white mb-1">Do something today that your future self will thank you for</h5>
+                  </div>
+                </div>
+                <div class="carousel-item h-100" style="background-image: url('theme/assets/img/carousel-2.jpg');
+      background-size: cover;">
+                  <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
+                    <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
+                      <i class="ni ni-bulb-61 text-dark opacity-10"></i>
+                    </div>
+                    <h5 class="text-white mb-1">I am proud to be an insurance agent, my job is to protect your family.</h5>
+                  </div>
+                </div>
+                <div class="carousel-item h-100" style="background-image: url('theme/assets/img/carousel-3.jpg');
+      background-size: cover;">
+                  <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
+                    <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
+                      <i class="ni ni-trophy text-dark opacity-10"></i>
+                    </div>
+                    <h5 class="text-white mb-1">Invest in yourself. You can afford it, trust me!</h5>
+                  </div>
+                </div>
+              </div>
+              <button class="carousel-control-prev w-5 me-3" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next w-5 me-3" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+      
+    </div>
     
      
     </div>
